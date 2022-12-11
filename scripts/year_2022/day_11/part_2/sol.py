e54204c2f87e83886f94942dab96c0fb33363f77
Ewen <ewenb1@gmail.com>
2022-12-11 14:54:05 +0000

    day 11

---
+++ scripts/year_2022/day_11/part_2/sol.py
@@ -0,0 +1,62 @@
+import math
+
+class Monkey:
+    def __init__(self, items, operation, decision):
+        self.items = items
+        self.operation = operation
+        self.decision = decision
+        self.inspections = 0
+    
+    def inspect(self, i):
+        self.items[i] = self.operation(self.items[i])
+        self.inspections += 1
+    
+    def throw(self, i):
+        reciever = self.decision(self.items[i])
+        return reciever
+
+    def startTurn(self):
+        for i in range(len(self.items)):
+            self.inspect(i)
+    
+    def endTurn(self):
+        self.items = []
+
+def getMonkeys():
+    m = []
+    m.append(Monkey([75, 75, 98, 97, 79, 97, 64], lambda old: old * 13, lambda new: 2 if new % 19 == 0 else 7))
+    m.append(Monkey([50, 99, 80, 84, 65, 95], lambda old: old + 2, lambda new: 4 if new % 3 == 0 else 5))
+    m.append(Monkey([96, 74, 68, 96, 56, 71, 75, 53], lambda old: old + 1, lambda new: 7 if new % 11 == 0 else 3))
+    m.append(Monkey([83, 96, 86, 58, 92], lambda old: old + 8, lambda new: 6 if new % 17 == 0 else 1))
+    m.append(Monkey([99], lambda old: old * old, lambda new: 0 if new % 5 == 0 else 5))
+    m.append(Monkey([60, 54, 83], lambda old: old + 4, lambda new: 2 if new % 2 == 0 else 0))
+    m.append(Monkey([77, 67], lambda old: old * 17, lambda new: 4 if new % 13 == 0 else 1))
+    m.append(Monkey([95, 65, 58, 76], lambda old: old + 5, lambda new: 3 if new % 7 == 0 else 6))
+    return m
+    
+
+def solution(input):
+    # input is hardcoded instead
+    monkeys = getMonkeys()
+    divThing = math.prod([19,3,11,17,5,2,13,7])
+    for c in range(10000):
+        if c % 100 == 0:
+            print(f"starting round {c}")
+
+        for monkey in monkeys:
+            monkey.startTurn()
+            for i in range(len(monkey.items)):
+                receiver = monkey.throw(i)
+                monkeys[receiver].items.append(monkey.items[i] % divThing)
+            monkey.endTurn()
+    
+    print("completed 10000 rounds")
+    for m in range(len(monkeys)):
+        print(f'Monkey {m} inspected items {monkeys[m].inspections} times.')
+    
+    inspections = [m.inspections for m in monkeys]
+    inspections.sort(reverse=True)
+
+    print(f"inspections: {inspections}")
+
+    return inspections[0]*inspections[1]
